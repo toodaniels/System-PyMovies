0774ac5ce86944ffbd1880d78bb7130f89019652
Daniel Torres <shaka_super@hotmail.com>
2016-10-01 18:35:46 -0500

    Update conectdb.py

--- Servidor/conectdb.py
+++ Servidor/conectdb.py
@@ -9,21 +9,26 @@ class pelicula(object): #creo clase pelicula
 			self.db =postgresql.open(query) #Conexión
 
 		def selectall(self): # Funcion para mostrar todos los registros
+			# el resutado lo guarda en una variable lista de la clase
 			self.lista = self.db.prepare("select * from pelicula ORDER BY id ASC")
 
 		def select(self, query): # Funcion ejecuta la busqueda que se le asigna por el servidor 
+			# el resutado lo guarda en una variable lista de la clase
 			self.lista = self.db.prepare(query + " ORDER BY id ASC")
 
 		def insert(self,titulo,director,genero,compra,venta,stock): #Inserta en la base de datos 
 			mkpeli=self.db.prepare("insert into pelicula(titulo,director,genero,compra,venta,stock) values ($1,$2,$3,$4,$5,$6)")
+			#noimporta si retorna por que no requerimos ver resultados 
 			mk = mkpeli(titulo,director,genero,compra,venta,stock)
 
 		def update(self,id,titulo,director,genero,compra,venta,stock): #Actualiza el regustro segun el id 
 			uppeli = self.db.prepare("update pelicula set titulo=$2,director=$3,genero=$4,compra=$5,venta=$6,stock=$7 where id = $1 or titulo = $2")
+			#noimporta si retorna por que no requerimos ver resultados 
 			up = uppeli(id,titulo,director,genero,compra,venta,stock)
 
 		def delete(self,id): #Elimina un registro segun su id  
 			rmpeli =self.db.prepare("delete from pelicula where id = $1 ")
+			#noimporta si retorna por que no requerimos ver resultados 
 			rm = rmpeli(id)
 			
 """
